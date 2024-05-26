4dc2ad2bcd3913cfa8c8d2eaa8f2110d4e76db85
Felipe E. Ribeiro <felipe.eduribeiro@gmail.com>
2024-05-26 20:28:50 -0300

    Nova versao 2.1

--- website/app.py
+++ website/app.py
@@ -14,7 +14,7 @@ def index():
     cur = mysql.connection.cursor()
     cur.execute("SELECT volume_number, is_owned FROM editions ORDER BY volume_number;")
     owned = cur.fetchall()
-    cur.execute("SELECT arc, GROUP_CONCAT(volume_number ORDER BY volume_number SEPARATOR ',') as volumes FROM editions GROUP BY arc ORDER BY MIN(volume_number)")
+    cur.execute("SELECT arc, GROUP_CONCAT(volume_number ORDER BY volume_number SEPARATOR ',') as volumes, count(volume_number), SUM(CASE WHEN is_owned = 1 THEN 1 ELSE 0 END) FROM editions GROUP BY arc ORDER BY MIN(volume_number)")
     editions = cur.fetchall()
     cur.close()
     return render_template("index.html", owned=owned, editions=editions, css_url=url_for('static', filename='style.css'))
@@ -25,7 +25,6 @@ def edit(id):
     cur.execute("SELECT volume_number,is_owned FROM editions WHERE volume_number = %s", [id])
     edition = cur.fetchone()
     if request.method == "POST":
-        #is_owned = request.form.get("is_owned", False)
         is_owned = bool(request.form.get("is_owned"))
         cur.execute("UPDATE editions SET is_owned = %s WHERE volume_number = %s", [is_owned, id])
         mysql.connection.commit()
@@ -34,6 +33,46 @@ def edit(id):
         return redirect(url_for('index'))
     cur.close()
     return render_template("edit.html", edition=edition, css_url=url_for('static', filename='style.css'))
+
+@app.route("/add/", methods=["GET", "POST"])
+def add():
+
+    if request.method == "POST":
+        arc = request.form.get("arc")
+        volume = request.form.get("volume")
+        if arc is not None and volume is not None:
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT volume_number FROM editions WHERE volume_number = %s", [volume])
+            edition = cur.fetchone()
+            if edition:
+                cur.close()
+                return redirect(url_for('error'))
+            else:
+                volume_before = int(volume) - 1
+                cur.execute("SELECT volume_number FROM editions WHERE volume_number = %s", [volume_before])
+                edition_before = cur.fetchone()
+                if edition_before is None:
+                    cur.close()
+                    return redirect(url_for('error'))
+                else:
+                    cur.execute("insert editions (volume_number, arc, is_owned) values (%s, %s, 0)", [volume, arc] )
+                    mysql.connection.commit()
+                    cur.close()
+                    return redirect(url_for('index'))
+    else:
+        return render_template("add.html", css_url=url_for('static', filename='style.css'))
+
+@app.route("/error/", methods=["GET", "POST"])
+def error():
+    return render_template("error.html", css_url=url_for('static', filename='style.css'))
+
+@app.errorhandler(500)
+def exception_handler(e):
+    return render_template('error.html'), 500
+
+@app.errorhandler(404)
+def exception_handler(e):
+    return render_template('error.html'), 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
